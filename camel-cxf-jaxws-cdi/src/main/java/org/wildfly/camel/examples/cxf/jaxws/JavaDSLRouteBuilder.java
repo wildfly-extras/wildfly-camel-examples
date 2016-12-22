39e1f30127e7a5628008caef83fed1863100249e
jochenr <jochenr@users.noreply.github.com>
2016-12-22 15:10:55 +0100

    push forgotten files

--- camel-cxf-jaxws-cdi/src/main/java/org/wildfly/camel/examples/cxf/jaxws/JavaDSLRouteBuilder.java
+++ camel-cxf-jaxws-cdi/src/main/java/org/wildfly/camel/examples/cxf/jaxws/JavaDSLRouteBuilder.java
@@ -1,9 +1,5 @@
 package org.wildfly.camel.examples.cxf.jaxws;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.ejb.Startup;
 import javax.enterprise.context.ApplicationScoped;
 
@@ -11,10 +7,6 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.cdi.ContextName;
 import org.apache.camel.component.cxf.CxfComponent;
 import org.apache.camel.component.cxf.CxfEndpoint;
-import org.apache.cxf.configuration.security.AuthorizationPolicy;
-import org.apache.cxf.interceptor.Interceptor;
-import org.apache.cxf.interceptor.security.JAASLoginInterceptor;
-import org.apache.cxf.message.Message;
 import org.wildfly.extension.camel.CamelAware;
 
 @Startup
@@ -32,18 +24,18 @@ public class JavaDSLRouteBuilder extends RouteBuilder {
     public void configure() throws Exception {
 
     	CxfComponent cxfConsumerComponent = new CxfComponent(getContext());
-    	CxfEndpoint cxfConsumerEndpoint = new CxfEndpoint("http://localhost:8080/webservices/greeting2", cxfConsumerComponent);
+    	CxfEndpoint cxfConsumerEndpoint = new CxfEndpoint("http://localhost:8080/webservices/greeting-cdi", cxfConsumerComponent);
     	cxfConsumerEndpoint.setBeanId("cxfConsumerEndpoint");
 //    	cxfFromEndpoint.setDataFormat(DataFormat.PAYLOAD);
     	cxfConsumerEndpoint.setServiceClass(org.wildfly.camel.examples.cxf.jaxws.GreetingService.class);
-    	List<Interceptor<? extends Message>> inInterceptors = cxfConsumerEndpoint.getInInterceptors();
-    	JAASLoginInterceptor jaasLoginInterceptor =  new JAASLoginInterceptor();
-    	jaasLoginInterceptor.setContextName("other");
-    	inInterceptors.add(jaasLoginInterceptor);
+//    	List<Interceptor<? extends Message>> inInterceptors = cxfConsumerEndpoint.getInInterceptors();
+//    	JAASLoginInterceptor jaasLoginInterceptor =  new JAASLoginInterceptor();
+//    	jaasLoginInterceptor.setContextName("other");
+//    	inInterceptors.add(jaasLoginInterceptor);
 
 
     	CxfComponent cxfProducerComponent = new CxfComponent(getContext());
-    	CxfEndpoint cxfProducerEndpoint = new CxfEndpoint("http://localhost:8080/webservices/greeting2", cxfProducerComponent);
+    	CxfEndpoint cxfProducerEndpoint = new CxfEndpoint("http://localhost:8080/webservices/greeting-cdi", cxfProducerComponent);
     	cxfProducerEndpoint.setBeanId("cxfProducerEndpoint");
     	cxfProducerEndpoint.setServiceClass(org.wildfly.camel.examples.cxf.jaxws.GreetingService.class);
 
@@ -55,19 +47,19 @@ public class JavaDSLRouteBuilder extends RouteBuilder {
     	// TODO: open JIRA ticket for
     	// org.apache.camel.component.cxf.CxfEndpoint
     	// Lines 554 -558 should be Nullsafe
-    	Map<String, Object> properties = cxfProducerEndpoint.getProperties();
-    	if (properties == null) {
-    		Map<String, Object> props = new HashMap<String, Object>();
-    		AuthorizationPolicy authPolicy = new AuthorizationPolicy();
-            authPolicy.setUserName("testUser");
-            authPolicy.setPassword("testPassword1+");
-//            factoryBean.getProperties().put(AuthorizationPolicy.class.getName(), authPolicy);
-            props.put(AuthorizationPolicy.class.getName(), authPolicy);
-    		cxfProducerEndpoint.setProperties(props);
-    	}
+//    	Map<String, Object> properties = cxfProducerEndpoint.getProperties();
+//    	if (properties == null) {
+//    		Map<String, Object> props = new HashMap<String, Object>();
+//    		AuthorizationPolicy authPolicy = new AuthorizationPolicy();
+//            authPolicy.setUserName("testUser");
+//            authPolicy.setPassword("testPassword1+");
+////            factoryBean.getProperties().put(AuthorizationPolicy.class.getName(), authPolicy);
+//            props.put(AuthorizationPolicy.class.getName(), authPolicy);
+//    		cxfProducerEndpoint.setProperties(props);
+//    	}
 
 
-    	from("direct:start2").to(cxfProducerEndpoint);
+    	from("direct:start").to(cxfProducerEndpoint);
 
     	from(cxfConsumerEndpoint).process(new GreetingsProcessor());
 
--- camel-cxf-jaxws-security/src/main/java/org/wildfly/camel/examples/cxf/jaxws/CamelCxfWsServlet.java
+++ camel-cxf-jaxws-security/src/main/java/org/wildfly/camel/examples/cxf/jaxws/CamelCxfWsServlet.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,6 +29,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.CamelExecutionException;
 import org.apache.camel.ProducerTemplate;
 
 @SuppressWarnings("serial")
@@ -64,7 +65,8 @@ public class CamelCxfWsServlet extends HttpServlet {
         Object[] serviceParams = new Object[] {message, name};
         String result = producer.requestBody("direct:start", serviceParams, String.class);
 
-        request.setAttribute("greeting", result);
+
+        request.setAttribute("greeting", result);        
         request.getRequestDispatcher("/greeting.jsp").forward(request, response);
     }
 }
