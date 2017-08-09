32e53aa1549d8297c1c8f58ed9583f24036f77e2
jochenr <jochenr@users.noreply.github.com>
2017-08-09 17:23:21 +0200

    Try to move CLI commands from maven to test class

--- itests/src/main/java/org/wildfly/camel/test/cxf/CxfSecureWsCdiExampleTest.java
+++ itests/src/main/java/org/wildfly/camel/test/cxf/CxfSecureWsCdiExampleTest.java
@@ -20,11 +20,21 @@
 package org.wildfly.camel.test.cxf;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.as.cli.CommandContext;
+import org.jboss.as.cli.CommandContextFactory;
+import org.jboss.as.cli.CommandFormatException;
+import org.jboss.as.cli.batch.Batch;
+import org.jboss.as.cli.batch.BatchManager;
+import org.jboss.as.cli.batch.BatchedCommand;
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.as.controller.client.helpers.Operations;
 import org.jboss.as.domain.management.security.adduser.AddUser;
+import org.jboss.dmr.ModelNode;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
@@ -34,11 +44,18 @@ import org.junit.runner.RunWith;
 import org.wildfly.camel.test.common.http.HttpRequest;
 import org.wildfly.camel.test.common.http.HttpRequest.HttpResponse;
 
-
 @RunWith(Arquillian.class)
 public class CxfSecureWsCdiExampleTest {
 
     private static final String ENDPOINT_ADDRESS = "http://localhost:8080/example-camel-cxf-jaxws-security-cdi/cxf/";
+
+    private static final String ADMIN_USER = "cliAdmin";
+    private static final String ADMIN_PASSWORD = "cliAdminPassword1+";
+
+    private static final String USER = "CN=localhost";
+    private static final String PASSWORD = "testPassword1++";
+    private static final String ROLE = "testRole";
+
 
     @Deployment
     public static WebArchive createDeployment() {
@@ -46,24 +63,103 @@ public class CxfSecureWsCdiExampleTest {
     }
 
     @BeforeClass
-    public static void testAddUser() throws Exception {
-        String[] commandLine = {"-a", "-s", "-u", "CN=localhost", "-p", "testPassword1+", "-g", "testRole"};
-        AddUser.main(commandLine);
+    public static void beforeClass() throws Exception {
+        addAppUser();
+        addManagementUser();
+        configureServerViaCLI();
     }
 
-    /**
-     * This Test will work after https://github.com/wildfly-extras/wildfly-camel/issues/1945 is fixed.
-     *
-     * @throws Exception
-     */
+
+
     @RunAsClient
     @Test
     public void testSayHelloCxfSoapRoute() throws Exception {
-        HttpResponse result = HttpRequest.post(ENDPOINT_ADDRESS)
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .content("message=Hello&name=Kermit")
-            .getResponse();
 
-        Assert.assertTrue(result.getBody().contains("Hello Kermit"));
+        //FIXME: very ugly, but unfortunately the CLI ":reload" has no "blocking" attribute;-(
+        Thread.sleep(10000);
+
+        HttpResponse result = HttpRequest.post(ENDPOINT_ADDRESS)
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .content("message=Hello&name=Kermit")
+                .timeout(30, TimeUnit.SECONDS)
+                .getResponse();
+
+            Assert.assertTrue(result.getBody().contains("Hello Kermit"));
     }
+
+    private static void addAppUser() {
+        String[] commandLine = {"-a", "-s", "-u", USER, "-p", PASSWORD, "-g", ROLE};
+        AddUser.main(commandLine);
+    }
+
+    private static void addManagementUser() {
+        String[] commandLine = { "-s", "-u", ADMIN_USER, "-p", ADMIN_PASSWORD};
+        AddUser.main(commandLine);
+    }
+
+    private static void configureServerViaCLI() throws Exception {
+
+        CommandContext ctx = CommandContextFactory.getInstance().newCommandContext(ADMIN_USER, ADMIN_PASSWORD.toCharArray());
+        ctx.connectController();
+
+        ModelControllerClient client = ctx.getModelControllerClient();
+
+        try {
+
+            // check if resources are already there
+            ModelNode checkAddress = Operations.createAddress("system-property", "javax.net.ssl.trustStore");
+            ModelNode checkOperation = Operations.createReadResourceOperation(checkAddress);
+            ModelNode checkResult = client.execute(checkOperation);
+            if (Operations.isSuccessfulOutcome(checkResult)) {
+                return;
+            } else {
+
+                BatchManager batchManager = ctx.getBatchManager();
+                if (!batchManager.isBatchActive()) {
+                    batchManager.activateNewBatch();
+                }
+                Batch activeBatch = batchManager.getActiveBatch();
+
+                BatchedCommand batchedCommand = ctx.toBatchedCommand("/system-property=javax.net.ssl.trustStore/:add(value=${jboss.home.dir}/standalone/configuration/application.keystore)");
+                activeBatch.add(batchedCommand);
+                batchedCommand = ctx.toBatchedCommand("/system-property=javax.net.ssl.trustStorePassword/:add(value=password)");
+                activeBatch.add(batchedCommand);
+
+                batchedCommand = ctx.toBatchedCommand("/subsystem=security/security-domain=certificate-trust-domain:add()");
+                activeBatch.add(batchedCommand);
+                batchedCommand = ctx.toBatchedCommand("/subsystem=security/security-domain=certificate-trust-domain/jsse=classic:add(truststore={\"password\"=>\"password\",\"url\"=>\"${jboss.home.dir}/standalone/configuration/application.keystore\"})");
+                activeBatch.add(batchedCommand);
+
+                batchedCommand = ctx.toBatchedCommand("/subsystem=security/security-domain=client-cert:add()");
+                activeBatch.add(batchedCommand);
+                batchedCommand = ctx.toBatchedCommand("/subsystem=security/security-domain=client-cert/authentication=classic:add(login-modules=[{\"code\"=\"CertificateRoles\",\"flag\"=\"required\",\"module-options\"=>[\"securityDomain\"=>\"certificate-trust-domain\",\"verifier\"=>\"org.jboss.security.auth.certs.AnyCertVerifier\",\"rolesProperties\"=>\"${jboss.home.dir}/standalone/configuration/application-roles.properties\"]}])");
+                activeBatch.add(batchedCommand);
+                batchedCommand = ctx.toBatchedCommand("/subsystem=undertow/server=default-server/https-listener=https/:write-attribute(name=verify-client,value=REQUESTED)");
+                activeBatch.add(batchedCommand);
+
+
+                ModelNode request = activeBatch.toRequest();
+                ctx.setResolveParameterValues(false);
+                ModelNode result = client.execute(request);
+                batchManager.discardActiveBatch();
+
+
+                if (Operations.isSuccessfulOutcome(result)) {
+                    ModelNode operation = Operations.createOperation(":reload");
+                    result = client.execute(operation);
+                } else {
+                    throw new RuntimeException("Failed to execute operation: " + request + " " + Operations.getFailureDescription(result).asString());
+                }
+
+            }
+        } catch (CommandFormatException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            client.close();
+            ctx.disconnectController();
+        }
+
+    }
+
 }
