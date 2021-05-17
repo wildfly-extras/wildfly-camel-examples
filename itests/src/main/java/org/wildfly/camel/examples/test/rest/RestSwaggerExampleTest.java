4046c6d9e076a30105f8007276a512bda79393c2
Thomas Diesler <tdiesler@redhat.com>
2021-05-17 12:23:44 +0200

    Ignore [ENTESB-16463] Jackson2 version incompatible with 7.4.0.GA

--- itests/src/main/java/org/wildfly/camel/examples/test/rest/RestSwaggerExampleTest.java
+++ itests/src/main/java/org/wildfly/camel/examples/test/rest/RestSwaggerExampleTest.java
@@ -30,6 +30,7 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.wildfly.camel.test.common.http.HttpRequest;
@@ -38,6 +39,7 @@ import org.wildfly.camel.test.common.utils.TestUtils;
 
 @RunAsClient
 @RunWith(Arquillian.class)
+@Ignore ("[ENTESB-16463] Jackson2 version incompatible with 7.4.0.GA")
 public class RestSwaggerExampleTest {
 
     @Deployment
